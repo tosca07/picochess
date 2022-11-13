ca5c1ef61010e8eb10d36e53050990a2446e0ab6
Gerhard Kalab <gkalab@users.noreply.github.com>
2022-11-13 20:05:37 +0100

    Fix type error for 'movestogo' when using tournament time controls

---
+++ tests/test_timecontrol.py
@@ -0,0 +1,11 @@
+import unittest
+
+from timecontrol import TimeControl, TimeMode
+
+
+class TestTimeControl(unittest.TestCase):
+
+    def test_uci_returns_int_for_movestogo(self):
+        tc = TimeControl(mode=TimeMode.BLITZ, moves_to_go=20)
+        tc.set_clock_times(5, 5, 10)
+        self.assertEqual(10, tc.uci()['movestogo'])
