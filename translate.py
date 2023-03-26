33878a089ea1f2ce9dd405b7eee6ca8dd29dfa8a
tosca07 <34959043+tosca07@users.noreply.github.com>
2023-03-26 20:44:02 +0200

    Update PicoChess text for wen

--- translate.py
+++ translate.py
@@ -658,7 +658,7 @@ class DgtTranslate(object):
             ittxt = Dgt.DISPLAY_TEXT(web_text='', large_text='uci960 si  ', medium_text='960 si  ', small_text='960 si')
         if text_id == 'picochess':
             wait = True
-            entxt = Dgt.DISPLAY_TEXT(web_text='', large_text=f'pChess {self.version}', medium_text=f'pico {self.version}', small_text=f'pic{self.version}')
+            entxt = Dgt.DISPLAY_TEXT(web_text=f'PicoChess {self.version}', large_text=f'pChess {self.version}', medium_text=f'pico {self.version}', small_text=f'pic{self.version}')
             detxt = entxt
             nltxt = entxt
             frtxt = entxt
