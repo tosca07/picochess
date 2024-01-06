c3f3f85d51d0249520e521d296a95c7870f582e9
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-01-05 23:06:54 -0500

    Update service and install script to use venv

--- install-picochess.sh
+++ install-picochess.sh
@@ -5,13 +5,16 @@
 
 apt update
 apt full-upgrade -y
-apt install git sox unzip wget python3-pip libtcl8.6 telnet -y
+apt install git sox unzip wget python3-pip libtcl8.6 telnet libglib2.0-dev -y
 
 pip3 install --upgrade pip
 
 cd /opt
 
 git clone https://github.com/ghislainbourgeois/picochess
+
+python3 -m venv /opt/picochess_venv
+source /opt/picochess_venv/bin/activate
 
 cd picochess
 
