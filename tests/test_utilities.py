e1ad7e1343e50fa2152dbfc3b094723034838f23
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-01-05 21:54:54 -0500

    Fix CI

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -8,8 +8,8 @@ class TestUtilities(unittest.TestCase):
     def test_engine_mame_par(self):
         self.assertEqual('-speed 1.0 -sound none', get_engine_mame_par(1.0))
         self.assertEqual('-speed 2.01', get_engine_mame_par(2.01, True))
-        self.assertEqual('-nothrottle -sound none', get_engine_mame_par(0.009))
-        self.assertEqual('-nothrottle', get_engine_mame_par(0.009, True))
+        self.assertEqual('-speed 30 -sound none', get_engine_mame_par(0.009))
+        self.assertEqual('-speed 30', get_engine_mame_par(0.009, True))
 
 
 if __name__ == '__main__':
