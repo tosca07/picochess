e5b1385216d9288e37a3938d306cc30f23e18724
JohanSjoblom <messier109@gmail.com>
2025-04-20 13:54:52 +0300

    Show score,depth from engine play
    - not pv as it was already working in BEST_MOVE
    - revert checking point of view score (wrong in previous commit)

--- picochess.py
+++ picochess.py
@@ -1089,7 +1089,8 @@ async def main() -> None:
                                 Event.BEST_MOVE(move=engine_res.move, ponder=engine_res.ponder, inbook=False)
                             )
                             if engine_res.info:
-                                await self.send_analyse(engine_res.info, True)  # fire PV, SCORE info, etc
+                                # send pv, score, not pv as its old pv[1] and sent by BEST_MOVE
+                                await self.send_analyse(engine_res.info, False)
                             # webplay: Event.BEST_MOVE pushes the move on display
                             # dgt board: BEST_MOVE 1) informs 2) user moves, 3) dgt event to process_fen() push
                     else:
@@ -2329,28 +2330,23 @@ async def main() -> None:
                 await self.send_analyse(info)
             return info
 
-        async def send_analyse(self, info: chess.engine.InfoDict, send_next_move: bool = False):
-            """send info - if engine_playing_moves use user color
-            otherwise its in analysing state and score is for turn
-            if send_next_move is true it sends the counter move pv[1]"""
-            # info is first multipv root move; send to displays
-            if "pv" in info:
-                # @todo check if we really have a move list here
-                if send_next_move:
-                    move = info.get("pv")[1]  # first counter move
-                else:
-                    move = info.get("pv")[0]  # first move
+        async def send_analyse(self, info: chess.engine.InfoDict, send_pv: bool = True):
+            """send pv, depth, and score events
+            with send_pv False pv message is not sent - use if its previous move InfoDict"""
+            # send depth before score as score is assembling depth in receiver end
+            if "depth" in info:
+                await Observable.fire(Event.NEW_DEPTH(depth=info.get("depth")))
+            if send_pv:
+                move = info.get("pv")[0] if "pv" in info else chess.Move.null()
                 if move:
                     self.state.pb_move = move  # backward compatibility
                     await Observable.fire(Event.NEW_PV(pv=[move]))
-            # send depth before score as score is assembling depth in receiver end
-            if "depth" in info:
-                d = info.get("depth")
-                if d:
-                    await Observable.fire(Event.NEW_DEPTH(depth=d))
             if "score" in info:
                 s = info["score"]
-                p = s.pov(self.state.game.turn).score()
+                if self.is_engine_playing_moves():
+                    p = s.pov(self.state.get_user_color()).score()
+                else:
+                    p = s.pov(self.state.game.turn).score()
                 if p:
                     await Observable.fire(Event.NEW_SCORE(score=p, mate=s.is_mate()))
 
